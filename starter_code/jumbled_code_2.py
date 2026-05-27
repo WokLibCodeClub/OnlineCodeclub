edf2ecf6bd0d6f35f06e99a4e27fecad5b09216e
simonc8 <simon@miltonrd.plus.com>
2026-05-27 16:36:40 +0100

    Create jumbled_code_2.py

---
+++ starter_code/jumbled_code_2.py
@@ -0,0 +1,20 @@
+     "BB   BB  ", "BB   BB  ", "BBBBBB   "]
+     "AA   AA  ", "AA   AA  ", "AA   AA  "]
+  print(print_line)
+  print_line = ""
+     "DD   DD  ", "DD   DD  ", "DDDDDD   "]
+B = ["BBBBBB   ", "BB   BB  ", "BB   BB  ", "BBBBBB   ",
+     "CC       ", "CC   CC  ", " CCCCC   "]
+
+    print_line = print_line + l[i]
+E = ["EEEEEEE  ", "EE       ", "EE       ", "EEEEEEE  ",
+C = [" CCCCC   ", "CC   CC  ", "CC       ", "CC       ",
+
+
+
+A = [" AAAAA   ", "AA   AA  ", "AA   AA  ", "AAAAAAA  ",
+for i in range(7):
+
+  for l in [A,B,C,D,E]:
+D = ["DDDDDD   ", "DD   DD  ", "DD   DD  ", "DD   DD  ",
+     "EE       ", "EE       ", "EEEEEEE  "]
