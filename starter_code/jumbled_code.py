3bbed1f9facdaa78708d7d28d0f690e74bbe064d
simonc8 <simon@miltonrd.plus.com>
2026-05-27 16:35:20 +0100

    Create jumbled_code.py

---
+++ starter_code/jumbled_code.py
@@ -0,0 +1,33 @@
+  draw_tri(sizes[n])
+          (31,-42),(75,32),(91,48),(110,43),(123,48)]
+          23, 34, -75, -26, 68, -75, 
+  t.penup()
+    
+t.speed(9)
+    else:
+sizes =  [-83, 23, 11, -19, 11, 
+
+    t.forward(abs(leng))
+from turtle import *
+t.penup()
+t.setheading(0)
+  for _ in range(3):
+          
+      t.left(120)
+for n in range(len(points)):
+t.hideturtle()
+          
+
+  t.goto(points[n])
+s.setup(400,350)
+  t.pendown()
+          -26, -23, -19, 15, 15]
+    if leng < 0:
+          (-85,44),(-86,-6),(-69,23),(-44,-42),(-28,-36),(6,23),
+t = Turtle()
+def draw_tri(leng):
+
+points = [(-140,44),(-134,44),(-117,27),(-108,12),(-91,27),
+      t.right(120)
+s = Screen()  
+    
