d5d6142d331fad575200133b8cc40baa2447e6ae
simonc8 <simon@miltonrd.plus.com>
2026-05-29 11:22:32 +0100

    Create file_saving.py

---
+++ starter_code/file_saving.py
@@ -0,0 +1,12 @@
+# this is just a dummy list of numbers to use to test
+# writing to a file
+time_series = [2,3,5,8,11,16,17,15,13,8,7,4,1,0]
+
+# print the time series list
+print(time_series)
+
+#out_string = str(time_series).strip('[]')
+
+#f = open("results.csv", "a")
+#f.write(out_string + "\n")
+#f.close()
